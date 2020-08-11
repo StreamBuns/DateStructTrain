26f0334cc723302d1cdad5b2ab78ef6b52fafe81
Streambuns <flyingpig@nwsuaf.edu.cn>
2020-08-11 17:59:32 +0800

    已经实现如果没有极端情况正常运行 bug在于当在高度>=2的时候 如果插入一个比里面任何一个数都大的树 search函数无法返回一个正常i;Insert函数正常运行  Bv0.1

--- B+Tree/B+Tree.h
+++ B+Tree/B+Tree.h
@@ -18,8 +18,8 @@ struct BPTreeNode
 {
     int n; //关键码的数量
     BPTreeNode * parent;//父结点指针
-    int key[m+1];//Key[0]未用
-    BPTreeNode * ptr[m+1];//子树结点指针数组，ptr[m]在一出事使用
+    int key[m+2];//Key[0]未用
+    BPTreeNode * ptr[m+2];//子树结点指针数组，ptr[m]在一出事使用
     BPTreeNode * right;//B+树叶结点指向右边相邻叶结点
     int * recptr[m+1];//每个索引项中指向数据区相应记录起始地址的指针
     BPTreeNode(){
@@ -29,10 +29,12 @@ struct BPTreeNode
             key[i]=MaxValue;
             ptr[i-1]=NULL;
         }
-//        ptr[m+1]=NULL;
+        ptr[m+1]=NULL;
         right = NULL;
     }
 };
+//---------------------------------------------------------------------------------
+//2020年 8月11日 星期二 16时59分15秒 CST
 
 struct Triple
 {
@@ -41,6 +43,7 @@ struct Triple
     //节点中关键码序号i
     //tag=0,搜索成功，tag=1,搜索不成功
 };
+//---------------------------------------------------------------------------------
 
 class BPTree
 {
@@ -48,7 +51,10 @@ private:
     BPTreeNode * root;//根指针
     BPTreeNode * sqt;
 public:
-    BPTree(){};//构造函数
+    BPTree(){
+        root = NULL;
+        sqt = NULL;
+    };//构造函数
     Triple Search(const int& x);//搜索
     bool Insert(const int& x);//插入关键码x
     bool Remove(const int& x);//删除关键码x
@@ -66,37 +72,173 @@ public:
         BPTreeNode * p = sqt;
         int i=0;
         while(p!=NULL){
-            for(i=2;i<=p->n;i++)
+            for(i=1;i<=p->n;i++)
                 std::cout<<p->key[i]<<std::endl;
             p=p->right;
         }
     
     }
 };
-void move(BPTreeNode*p,BPTreeNode*q,int s,int x );
-void insertKey(BPTreeNode* p,int j,int k,BPTreeNode*bp);
+
+
+//---------------------------------------------------------------------------------
+//Created by 赵桐 on 2020年 8月11日 星期二 16时59分15秒 CST
+/*
+*把关键码k插入到p的key[j+1]的位置，需要先把后面的移动一个然后再插入
+*同理也是把ap插入到p的ptr[j+1]的位置
+*/
+void insertKey(BPTreeNode* p,int j,int k,BPTreeNode* ap){
+    int i,x=p->n;
+    for(i=0;i<(p->n)-j;i++){
+        p->key[x+1]=p->key[x];
+        p->ptr[x+1]=p->ptr[x];
+        x--;
+    }
+    p->key[j+1]=k;
+    p->ptr[j+1]=ap;
+    p->n++;
+}
+//---------------------------------------------------------------------------------
+//Created by 赵桐 on 2020年 8月11日 星期二 16时59分15秒 CST
+/*
+*分裂函数
+*把p的右指针指向新分裂出来的q
+*把p的key[s+2,m+1]移动到q的key[1,s]
+*把p的ptr[s+1,m+1]移动到q的key[0,s]
+*p->n变为s+1，q->n变为m-s
+*/
+//---------------------------------------------------------------------------------
+void move(BPTreeNode*p,BPTreeNode*q,int s,int x ){
+    int i=0,j=s+2;
+    for(i=0;i<s+1;i++){
+        q->key[i+1]=p->key[j];
+        q->ptr[i]=p->ptr[j-1];
+        j++;
+    }
+    q->ptr[i]=p->ptr[j-1];
+    p->n=s+1;
+    q->n=x-s;
+}
+//---------------------------------------------------------------------------------
+//Created by 赵桐 on 2020年 8月11日 星期二 16时59分15秒 CST
+/*
+*分裂函数
+*把p的右指针指向新分裂出来的q
+*把p的key[s+2,m+1]移动到q的key[1,s]
+*把p的ptr[s+1,m+1]移动到q的key[0,s]
+*p->n变为s+1，q->n变为m-s
+*/
+//---------------------------------------------------------------------------------
 bool BPTree::Insert(const int&x){
     
     Triple loc = Search(x);
-    if(!loc.tag)return false;
-    
+    if(!loc.tag)return false;//先搜索如果有就不用插入
     BPTreeNode *p =loc.r,*q;
     BPTreeNode *ap=NULL,*t;
-    int k=x,j=loc.i;
-    if(p->n<m){
-        insertKey(p, j, k, ap);
+    if(loc.r==NULL){
+        sqt = root=new BPTreeNode();
+        insertKey(root, 0, x, ap);
         return true;
     }
-    int s=(m+1)/2;
-    q=new BPTreeNode();
+
+    int k=x,j=loc.i,k1,i=0;
+    //k是要插入的关键码，j是插入的位置，k1是存储原来p的最大值（用来判断是否刷新父树上的p的最大值），i用来遍历，
     
+    //如果节电p的个数小于m那么可以直接插入，但是需要判断，插入的是不是这个分组中最大的数，
+    //如果是最大的数且有父节点，那么需要把父节点相应的之前p节点最大的数换成现在p节电最大的数
+    if(p->n<m){
+        if(k<p->key[p->n]||p->parent==NULL)
+        insertKey(p, j, k, ap);
+        else{
+            insertKey(p, j, k, ap);
+            while(p->parent->key[i]!=p->key[p->n])i++;//定位到父节点相应的与之前p中最大值相等的位置，换成现在p中的最大值
+            p->parent->key[i]=k;
+            
+        }
+        return true;
+    }
+    
+    int s=(m+1)/2;
+    k1=p->key[p->n];   //k1存储现在插入分裂前的时候的p的最大值
+    insertKey(p, j, k, ap);
+    q=new BPTreeNode();
+    p->right=q;    //把p的右指针指向新分裂出来的q
+    move(p,q,s,m);//把p的key[s+2,m+1]移动到q的key[1,s]
+                  //把p的ptr[s+1,m+1]移动到q的key[0,s]
+                  //p->n变为s+1，q->n变为m-s
+    
+
+    k=q->key[q->n];     //k存储q当前的最大的key
+    ap=q;
+    if(p->parent!=NULL){
+        t=p->parent;
+        i=j=0;
+        
+        while(t->key[i+1]==k1)j++;
+        t->key[i+1]=p->key[s+1];
+        //找到与之前p的最大key值相等的key，并且替换成现在p中最大的值
+        while(t->key[j+1]<k)j++;
+        //找到现在的q的最大key值要插入的地方
+        q->parent=p->parent;
+        p=t;
+    }
+    else{
+        root = new BPTreeNode();
+        root->n=2;                           //加入两个key
+        root->parent=NULL;                   //创建新的根
+        root->key[1]=p->key[p->n];root->key[2]=q->key[q->n];     //把子树的两个最大值整进去
+        root->ptr[0]=p;root->ptr[1]=q;       //把子节点连接到p，和新分裂的q
+        sqt=p;                              //p就是以后无论如何最小的那个组，
+                                            // 所以把左指针给它
+        return true;
+    }
     
     return true;
 }
-
-Triple BPTree::Search(const int&x){
+//---------------------------------------------------------------------------------
+/*
+*Triple Search ( const int& x );
+*
+*create by FlyingMaster Create on 2020年 8月11日 星期二 17时08分28秒 CST
+*
+*用x关键码搜索
+*返回一个类型为Triple(r,i,tag)的记录
+*tag=0，表示x在节点r找到
+*tag=1，表示没有找到x，这时可以插入的节点是r
+*插入到该结点的K[i]与K[i+1]之间
+*
+*/
+//---------------------------------------------------------------------------------
+Triple BPTree::Search (const int& x) {
     Triple result;
-    
-    return result;
+    //GetNode(root);
+    BPTreeNode *p = root, *q = NULL;
+    int i = 0;
+    while (p != NULL){
+        i = 0;
+        p->key[p->n+1] = MaxValue; //将Key值最大的后一个Key值设为MaxValue
+                                 //以防止找不到相应的小于x的Key值
+        while ( p->key[i + 1] < x )
+            i++;
+        if (p->key[i + 1] == x)  {
+            while (p != NULL) {
+                p = p->ptr[p->n]; //如果在非叶子节点找到等于x的key值
+                                  //直接遍历叶子结点
+                i = p->n;         //更新i使其等于n
+            }
+            result.r = p;
+            result.i = i + 1;
+            result.tag = 0;
+            return result;
+        }
+        q = p;
+        p = p->ptr[i];
+        //GetNode(p);
+    }
+    result.r = q;
+    result.i = i;
+    result.tag = 1;
+    return result;//搜索失败，返回插入位置
+                  //x可能落入的区间K[i]与K[i+1]
 }
 #endif /* B_Tree_h */
--- B+Tree/main.cpp
+++ B+Tree/main.cpp
@@ -11,7 +11,23 @@
 int main(int argc, const char * argv[]) {
     // insert code here...
     BPTree * bp = new BPTree();
-    
+    bp->Insert(01);
+    bp->Insert(24);
+    bp->Insert(39);
+    bp->Insert(72);
+    bp->Insert(53);
+     bp->Insert(63);
+    bp->Insert(71);
+//     bp->Insert(90);
+//     bp->Insert(88);
+//    bp->Insert(15);
+    bp->Travelsal();
+//    int n=4;
+//    int array[m+1];
+//
+//    for (int i=0; i<=m+1; i++) {
+//        cout<<array[i]<<endl;
+//    }
     std::cout << "Hello, World!\n";
     return 0;
 }
